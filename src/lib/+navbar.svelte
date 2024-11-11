b4ae5ec11d0728c3236f6e7e6fffa068a01dc837
shwntypar <shawnrada4@gmail.com>
2024-11-11 13:06:26 +0800

    inital commit

--- src/lib/+navbar.svelte
+++ src/lib/+navbar.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { base } from '$app/paths';
 
     export let tabs = [
         { name: "Shop", link: "/project2/Shop" },
@@ -6,15 +7,14 @@
     ];
 
     let dropdown = false;
-    
 </script>
 
-<div class="w-full flex px-10 justify-between items-center  bg-slate-300 h-20 shadow-md absolute top-0 z-10">
-    <a href="/">
-        <p class=" drop-shadow-md font-bold text-4xl text-slate-100"><span class=" text-yellow-300">TASTE</span> HAVEN</p>
+<div class="w-full flex px-10 justify-between items-center bg-slate-300 h-20 shadow-md absolute top-0 z-10">
+    <a href="{base}/">
+        <p class="drop-shadow-md font-bold text-4xl text-slate-100"><span class="text-yellow-300">TASTE</span> HAVEN</p>
     </a>
-    <div class=" space-x-2">
-        <a class=" rounded-full drop-shadow-sm font-semibold text-white border-2 border-white hover:bg-red-900 px-4 py-3" href="{tabs[0].link}">{tabs[0].name}</a>
-        <a class=" rounded-full drop-shadow-sm font-semibold text-white border-2 border-white hover:bg-red-900 px-4 py-3" href="{tabs[1].link}">{tabs[1].name}</a>
+    <div class="space-x-2">
+        <a class="rounded-full drop-shadow-sm font-semibold text-white border-2 border-white hover:bg-red-900 px-4 py-3" href="{tabs[0].link}">{tabs[0].name}</a>
+        <a class="rounded-full drop-shadow-sm font-semibold text-white border-2 border-white hover:bg-red-900 px-4 py-3" href="{tabs[1].link}">{tabs[1].name}</a>
     </div>
 </div>
